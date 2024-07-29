d0ed5ddeaeb2cf34933b5bb968c58efb7861a43c
Bradyn <bradyn.lv@gmail.com>
2024-07-29 13:29:29 -0700

    Finished

--- index.js
+++ index.js
@@ -1,30 +1,72 @@
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3) + 1
-    console.log(computerChoice)
+
+    if (computerChoice === 1) {
+        console.log("Computer chose Rock")
+    } else if (computerChoice === 2) {
+        console.log("Computer chose Paper")
+    } else {
+        console.log("Computer chose Scissors")
+    }
+
+    return computerChoice
 }
 
-getComputerChoice()
-
-let humanChoice = window.prompt("Rock, Paper, or Scissors?")
-
 function getHumanChoice() {
-    choice = ""
+    let humanChoice = window.prompt("Rock, Paper, or Scissors?")
+    humanChoice = humanChoice.toLowerCase()
 
     if (humanChoice === "rock") {
         console.log("Player chose Rock")
-        choice = 1
+        humanChoice = 1
+        return humanChoice
     } else if (humanChoice === "paper") {
         console.log("Player chose Paper")
-        choice = 2
+        humanChoice = 2
+        return humanChoice
     } else if (humanChoice === "scissors") {
         console.log("Player chose Scissors")
-        choice = 3
+        humanChoice = 3
+        return humanChoice
     } else {
         console.log("Please pick a choice")
     }
 }
 
-getHumanChoice()
-
 let humanScore = 0
 let computerScore = 0
+
+function playRound(humanChoice, computerChoice) {
+    let game = ""
+
+    if (humanChoice === 1 && computerChoice === 3 ||
+        humanChoice === 2 && computerChoice === 1 ||
+        humanChoice === 3 && computerChoice === 2
+    ) {
+        console.log("You win!")
+        humanScore++
+    } else if 
+       (humanChoice === 1 && computerChoice === 2 ||
+        humanChoice === 2 && computerChoice === 3 ||
+        humanChoice === 3 && computerChoice === 1
+    ) {
+        console.log("You lose!")
+        computerScore++
+    } else {
+        console.log("It's a draw!")
+        computerScore++
+        humanScore++
+    }
+}
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice()
+        const computerSelection = getComputerChoice()
+        playRound(humanSelection, computerSelection)
+        console.log(`Player score: ${humanScore}`)
+        console.log(`Computer score: ${computerScore}`)
+    }
+}
+
+playGame()
